25a100f9b66df0d00a9c128f6da4295a60fbf681
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2022-01-27 13:52:11 +0100

    Add files via upload

---
+++ Adventure/src/com/shaker/Location.java
@@ -0,0 +1,34 @@
+package com.shaker;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+        this.exits.put("Q", 0); // to quit the game
+    }
+
+    public void addExit(String direction, int location){
+        exits.put(direction,location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
